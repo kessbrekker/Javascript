31a6e160717a91f9f612f3719cbba7638d024129
Kess <76697188+kessbrekker@users.noreply.github.com>
2025-04-07 18:13:46 +0300

    Sayı Üretme

--- js/adet.js
+++ js/adet.js
@@ -1,5 +1,5 @@
 function adet(){
-    let a=document.getElementById("adet").value;
+    let a=parseInt(document.getElementById("adet").value);
     for(i=1;i<a;i++){
         document.getElementById("sonuc2").innerHTML=i+" kadar sayı üretildi"
     }
