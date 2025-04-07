01892f57140fe49901886f49ba7f881c14dec0e0
Kess <76697188+kessbrekker@users.noreply.github.com>
2025-04-07 18:15:59 +0300

    Javascript

--- js/adet.js
+++
@@ -1,6 +0,0 @@
-function adet(){
-    let a=parseInt(document.getElementById("adet").value);
-    for(i=1;i<a;i++){
-        document.getElementById("sonuc2").innerHTML=i+" kadar sayı üretildi"
-    }
-}
